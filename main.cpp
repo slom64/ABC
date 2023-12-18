3775e56f19cf3f1ab7b00bb70ef70d16a8b5ee99
slom64 <141697945+slom64@users.noreply.github.com>
2023-12-18 20:05:58 +0200

    1 (#1)

--- main.cpp
+++ main.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 
 
-int my_sum2(int a, int b, int c);
+
+int my_sum1(int a, int b, int c);
+
 
 
 int main()
 {
-	cout << my_sum2(2, 4, 6);
+
+	cout << my_sum1(2, 4, 6);
 	return 0;
 }
 
 
 
-int my_sum2(int a, int b, int c)
+
+int my_sum1(int a, int b, int c)
 {
 	return (a + b + c);
 }
